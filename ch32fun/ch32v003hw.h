e5460cb7e75516169d67ed0cb9acd096296f773b
lopple <5253526+lopple@users.noreply.github.com>
2026-06-27 10:47:07 +0900

    ch32v003: expose GPIOMode IPU/IPD aliases

--- ch32fun/ch32v003hw.h
+++ ch32fun/ch32v003hw.h
@@ -3284,18 +3284,7 @@ typedef enum
 	GPIO_Speed_30MHz = 3, // Alias 30MHz to 50MHz.
 } GPIOSpeed_TypeDef;
 
-#endif
-
-#define GPIO_CNF_IN_ANALOG   0
-#define GPIO_CNF_IN_FLOATING 4
-#define GPIO_CNF_IN_PUPD     8
-#define GPIO_CNF_OUT_PP      0
-#define GPIO_CNF_OUT_OD      4
-#define GPIO_CNF_OUT_PP_AF   8
-#define GPIO_CNF_OUT_OD_AF   12
-
 /* Configuration Mode enumeration */
-/*
 typedef enum
 {
     GPIO_Mode_AIN = 0x0,
@@ -3307,7 +3296,16 @@ typedef enum
     GPIO_Mode_AF_OD = 0x1C,
     GPIO_Mode_AF_PP = 0x18
 } GPIOMode_TypeDef;
-*/
+
+#endif
+
+#define GPIO_CNF_IN_ANALOG   0
+#define GPIO_CNF_IN_FLOATING 4
+#define GPIO_CNF_IN_PUPD     8
+#define GPIO_CNF_OUT_PP      0
+#define GPIO_CNF_OUT_OD      4
+#define GPIO_CNF_OUT_PP_AF   8
+#define GPIO_CNF_OUT_OD_AF   12
 
 #ifndef __ASSEMBLER__
 
